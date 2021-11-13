d7b71867a237eabd13a0d9bbfc58c7c6e87ce7d0
ROSANNA PISTONE <rosanna@pistone.se>
2021-11-13 18:08:01 +0100

    Finally magage to change background color onclick

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
-function changeBackgroundToExitPage() {
- document.body.getElementsByTagName("main").style.backgroundImage = "url(../Assets/underWater.jpg)";
-    
+  //Exit page color
+  let color = ['black'];
 
-}
+  //Get button
+  let  button = document.getElementById('first-view-button-no');
+
+  //Add event listener
+  button.addEventListener('click', function (){
+    
+    // Tilldelning
+    var exitColor = color;
+
+    //Get main-background
+    let background = document.getElementById('main-background');
+
+    //Background styling
+      background.style.background = exitColor;
+
+  })
