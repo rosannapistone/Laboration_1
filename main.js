4550572b335aeaa40feefe348fe521bb8ce91ba1
ROSANNA PISTONE <rosanna@pistone.se>
2021-11-14 15:01:22 +0100

    Add input field

--- main.js
+++ main.js
@@ -1,19 +1,49 @@
   //Exit page color
-  let color = ['black'];
+  const exitBackgroundImage = "url('/Assets/underWater.jpg')";
+  const inputNameBackgroundImage = "url('/Assets/underWater.jpg')";
 
   //Get button
-  let  button = document.getElementById('first-view-button-no');
+  const noButton = document.getElementById('first-view-button-no');
+  const yesButton = document.getElementById('first-view-button-yes');
+  const questionText = document.getElementById('question-text');
+  const questionContainer = document.getElementById('question-container');
+  const inputUserName = document.createElement("input");
+  const buttonContainer = document.getElementById('button-container');
+  const submitButton = document.createElement("button");
 
   //Add event listener
-  button.addEventListener('click', function (){
-    
-    // Tilldelning
-    var exitColor = color;
-
+  noButton.addEventListener('click', function (){
     //Get main-background
-    let background = document.getElementById('main-background');
-
+    const background = document.getElementById('main-background');
     //Background styling
-      background.style.background = exitColor;
-
+      background.style.backgroundImage = exitBackgroundImage;
   })
+
+ 
+
+    //Add event listener
+    yesButton.addEventListener('click', function (){
+        //Get main-background
+        const background = document.getElementById('main-background');
+        //Background styling
+          background.style.backgroundImage = inputNameBackgroundImage;
+          yesButton.style.display = 'none';
+          noButton.style.display = 'none';
+          questionText.style.display = 'none';          
+          questionContainer.appendChild(inputUserName);
+          
+          submitButton.className = 'buttons';
+          submitButton.style.backgroundColor = 'green';
+          submitButton.textContent = 'Save';
+          buttonContainer.appendChild(submitButton);
+      })
+
+      submitButton.addEventListener('click', function (){
+        console.log(inputUserName.value);
+    })
+
+      /* const div = document.createElement("div");
+          div.style.width = '5rem';
+          div.style.height = '5rem';
+          div.style.backgroundColor = 'red';
+          textContainer.appendChild(div);*/
